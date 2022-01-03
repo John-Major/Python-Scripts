a5a5a2cb2066dcd84c4054087af01177e2dc3d88
John Major <johnpmajor96@gmail.com>
2022-01-03 14:43:49 -0500

    Add files via upload

--- json_from_api.py
+++ json_from_api.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-test = 'https://api.polygon.io/v1/meta/symbols/AAPL/company?apiKey=MNExhabeDDgHYLqKlxDoT79JUdvT_OaI'
+test = 'api key + website'
 
 response = requests.get(test)
 json_response = response.json()
